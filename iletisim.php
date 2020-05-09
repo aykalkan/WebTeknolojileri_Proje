71a822ca75597ab003353a57313306c93c43a609
Ahmet Yasin Kalkanlı - G191210031 <g191210031@sakarya.edu.tr>
2020-05-09 19:48:07 +0300

    İletişim formunun php ile işlenmesi tamamlandı

--- iletisim.php
+++ iletisim.php
@@ -1,26 +1,90 @@
 <?php
-    $ad = $_POST["ad"];
-    $sifre = $_POST["sifre"];
-    $cinsiyet = $_POST["cinsiyet"]=="1" ? "Erkek":"Kadın";
+    $email = $_POST["email"];
+    $isim = $_POST["isim"];
+    $cinsiyet = $_POST["cinsiyet"]=="e" ? "Erkek":"Kadın";
+    $sayfa = $_POST["sayfa"];
+    $sebep = $_POST["sebep"];
+    $konu = $_POST["konu"];
+    $mesaj = $_POST["mesaj"];
+    
+    $secili_sayfalar = " ";
+    if(empty($sayfa)) 
+    {
+      $secili_sayfalar = "Herhangi bir sayfa dolaşmamışsınız.";
+    } 
+    else 
+    {
+        foreach($sayfa as $s)
+        {
+            $secili_sayfalar .= $s.", ";
+        }
+        $secili_sayfalar = substr($secili_sayfalar, 0, -2);
 
-    echo "Adınız : ".$_POST["ad"]."<br>";
-    echo "Şifreniz : ".$_POST["sifre"]."<br>";
-    echo ($_POST["cinsiyet"]=="1" ? "Erkek":"Kadın")."<br>";
+    }
+
+    $secili_sebep=" ";
+    switch ($sebep) {
+        case '0':
+            $secili_sebep = "Teşekkür";
+            break;
+        
+        case '1':
+            $secili_sebep = "Şikayet";
+            break;
+        
+        case '2':
+            $secili_sebep = "Hal hatır sorma";
+            break;
+        
+        case '3':
+            $secili_sebep = "İş";
+            break;
+        
+        case '4':
+            $secili_sebep = "Diğer";
+            break;
+        
+        default:
+            # code...
+            break;
+    }
+
+
 ?>
 
-<br><p style="font-size: large;margin: 20;"> Mesajınız başarıyla alındı. En kısa zamanda dönüş yapılacaktır.</p></br>
+<br><p style="font-size: large;margin: 20;">Mesajınız başarıyla alındı. En kısa zamanda dönüş yapılacaktır.</p></br>
 
-<table border="1" width="200" height="40">
+<table border="1" width="500">
     <tr>
         <td>Adınız:</td>
-        <td><?php echo $ad ?></td>
+        <td><?php echo $isim ?></td>
     </tr>
     <tr>
-        <td>Sifre:</td>
-        <td><?php echo $sifre ?></td>
+        <td>E-postanız:</td>
+        <td><?php echo $email ?></td>
     </tr>
     <tr>
         <td>Cinsiyet:</td>
         <td><?php echo $cinsiyet ?></td>
     </tr>
+    <tr>
+        <td>Gezdiğiniz sayfalar:</td>
+        <td><?php echo $secili_sayfalar ?></td>
+    </tr>
+    <tr>
+        <td>İletişim sebebiniz:</td>
+        <td><?php echo $secili_sebep ?></td>
+    </tr>
+    <tr>
+        <td>Konu:</td>
+        <td><?php echo $konu ?></td>
+    </tr>
+    <tr>
+        <td>Mesajınız:</td>
+        <td><?php echo $mesaj ?></td>
+    </tr>
 </table>
+
+<br>
+
+<a href="iletisim.html">Siteye geri dön</a>
